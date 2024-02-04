d037ecddf31ecb670c523954f756f017242a4f1a
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2024-02-04 23:05:46 +0200

    attempt-1

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const color = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+function buttonHandler() {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = body.style.backgroundColor;
+}
+
+button.addEventListener('click', buttonHandler);
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+function createBoxes() {
+  const numberOfBoxes = input.value;
+  console.log(numberOfBoxes);
+
+  if (numberOfBoxes <= 0 || numberOfBoxes > 100) {
+   alert('Please enter a valid number greater than 0 and no more than 100.');
+    return;
+  }
+
+  clearBoxes();
+
+  for (let i = 0; i < numberOfBoxes; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+
+  input.value = ''; // Очистка значення інпуту
+}
+
+  
+function clearBoxes() {
+  while (boxesContainer.firstChild) {
+    boxesContainer.removeChild(boxesContainer.firstChild);
+  }
+}
+
+function destroyBoxes() {
+  clearBoxes();
+  input.value = '';
+}
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
